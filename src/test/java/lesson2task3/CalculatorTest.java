bd647076bb1f089a3b89b89e97e3232abaed69d4
Patrycja <patiwojcik1@gmail.com>
2022-12-23 17:53:28 +0100

    Zaimplementowano zadanie bez testow

--- src/test/java/lesson2task3/CalculatorTest.java
+++ src/test/java/lesson2task3/CalculatorTest.java
@@ -1,4 +1,25 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lesson2task3;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class CalculatorTest {
-  
+
+    @Test
+    void shouldAddTwoNumber() {
+        int result = Calculator.sum(2, 2);
+        Assertions.assertEquals(4, result);
+    }
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void divide() {
+    }
 }
