dd374509380706c06fd3002a2552847823798428
Patrycja <patiwojcik1@gmail.com>
2022-12-29 18:24:59 +0100

    rozwiazano zadanie 5 z lekcji 2

---
+++ src/main/java/lesson2task5/Numbers.java
@@ -0,0 +1,20 @@
+package pl.futurecollars.lekcja2.zadanie4;
+
+public class Numbers {
+    public static void main(String[] args) {
+        System.out.println(isAnEvenNumber(10));
+    }
+
+    public static boolean isAnEvenNumber(int value);
+
+    {
+        if (value / 2) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+
+}
