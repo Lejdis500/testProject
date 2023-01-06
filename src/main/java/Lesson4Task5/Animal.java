555298e25d34bb2a37b9775fc7de3435b91087fa
Patrycja <patiwojcik1@gmail.com>
2023-01-06 16:54:26 +0100

    Utworzono klase Animal

---
+++ src/main/java/Lesson4Task5/Animal.java
@@ -0,0 +1,10 @@
+package Lesson4Task5;
+
+public class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/main/java/Lesson4Task5/Dog.java
@@ -0,0 +1,7 @@
+package Lesson4Task5;
+
+public class Dog extends Animal {
+    public Dog(String name) {
+        super(name);
+    }
+}
