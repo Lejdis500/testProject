49fd27eab11d7a7c2fe63c465766ab30e042ad3b
Patrycja <patiwojcik1@gmail.com>
2023-01-07 22:57:57 +0100

    Utworzono klase Movie

--- src/main/java/Lesson3Task2/Main.java
+++ src/main/java/Lesson3Task2/Main.java
@@ -2,8 +2,7 @@ package Lesson3Task2;
 
 public class Main {
     public static void main(String[] args) {
-        Movie movie = new Movie("The crown", "Jan Kowalski", 150,
-                2010);
+        Movie movie = new Movie("The crown", "Jan Kowalski", 150, 2010);
     }
 }
 
