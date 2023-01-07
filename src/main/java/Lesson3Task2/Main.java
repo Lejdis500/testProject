46911817168747926f624117fb80b7072e4ae017
Patrycja <patiwojcik1@gmail.com>
2023-01-07 22:55:17 +0100

    Utworzono klase Main

--- src/main/java/Lesson3Task2/Main.java
+++ src/main/java/Lesson3Task2/Main.java
@@ -2,7 +2,8 @@ package Lesson3Task2;
 
 public class Main {
     public static void main(String[] args) {
-        Movie movie = new Movie("The crown", "Jan Kowalski", 150, 2010);
+        Movie movie = new Movie("The crown", "Jan Kowalski", 150,
+                2010);
     }
 }
 
