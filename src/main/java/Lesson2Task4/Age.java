795aa6bd6ebe61efd168e4ac6ff024d19ab53e0a
Patrycja <patiwojcik1@gmail.com>
2023-01-10 20:34:26 +0100

    Utworzono klase Age

--- src/main/java/Lesson2Task4/Age.java
+++ src/main/java/Lesson2Task4/Age.java
@@ -2,17 +2,18 @@ package Lesson2Task4;
 
 public class Age {
     public static boolean isAdult(int age) {
-        if (age == 18) {
-            return true;
-        } else if (age > 18) {
+        if (age >= 18) {
             return true;
         } else if (age < 18) {
             return false;
-        } else {
 
-        }
-    }
+        }}}
 
-}
+
+
+
+
+
+
 
 
