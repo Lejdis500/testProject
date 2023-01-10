8a5adfb6445f0a501abc5b30606aa325203c2651
Patrycja <patiwojcik1@gmail.com>
2023-01-10 19:54:08 +0100

    Utworzono klase DayOfWeek

---
+++ src/main/java/Lesson3Task1/Calculator.java
@@ -0,0 +1,8 @@
+package Lesson3Task1;
+
+public class Calculator {
+    public int add (int a, int b) {
+        return a +b ;
+
+    }
+}
---
+++ src/test/java/Lesson3Task1/CalculatorTest.java
@@ -0,0 +1,7 @@
+package Lesson3Task1;
+
+import junit.framework.TestCase;
+
+public class CalculatorTest extends TestCase {
+
+}
