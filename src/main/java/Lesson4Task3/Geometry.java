bfe5588c805053d6704a4598f7752fb46ba75c98
Patrycja <patiwojcik1@gmail.com>
2023-01-11 17:44:37 +0100

    Utworzono klasę Geometry

--- src/main/java/Lesson4Task3/Geometry.java
+++ src/main/java/Lesson4Task3/Geometry.java
@@ -1,21 +1,9 @@
 package Lesson4Task3;
 
 public interface Geometry {
-    public static int areaOfSquare(int sideA) {
-        return sideA * sideA;
-    }
+    int getArea();
 
-    public static int fieldOfSquare(int sideA) {
-        return 4 * sideA;
-    }
-
-    public static int areaOfRectangle(int sideA, int sideB) {
-        return sideA * sideB;
-    }
-
-    public static int fieldOfRectangle(int sideA, int sideB) {
-        return 2 * sideA + 2 * sideB;
-    }
+    int getPerimeter();
 
 }
 
--- src/main/java/Lesson4Task3/Square.java
+++ src/main/java/Lesson4Task3/Square.java
@@ -1,6 +1,21 @@
 package Lesson4Task3;
 
-public class Square implements Geometry{
+public class Square implements Geometry {
 
+    private int sideA;
 
+    public Square(int sideA) {
+        this.sideA = sideA;
+    }
+
+    @Override
+    public int getArea() {
+        return sideA * sideA;
+
+    }
+
+    @Override
+    public int getPerimeter() {
+        return 4 * sideA;
+    }
 }
