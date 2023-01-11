424d71eeb94ebb7d9131f9c1f289c4a2cb794aa4
Patrycja <patiwojcik1@gmail.com>
2023-01-11 12:48:38 +0100

    Utworzono klase Triangle.java

---
+++ src/main/java/Lesson5Task9/Triangle.java
@@ -0,0 +1,16 @@
+package Lesson5Task9;
+
+public class Triangle {
+
+    width;
+    private
+
+    @Override
+    public String toString() {
+        return "Triangle{width=<wartośćPolaWitdh>, height=<wartośćPolaHeight>}";
+    }
+
+
+
+
+}
