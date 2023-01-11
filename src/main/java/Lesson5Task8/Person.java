2c706c8606aea0c3c21b897815fa624fc9691330
Patrycja <patiwojcik1@gmail.com>
2023-01-11 12:36:11 +0100

    Utworzono klase Person.java

---
+++ src/main/java/Lesson5Task8/Person.java
@@ -0,0 +1,23 @@
+package Lesson5Task8;
+
+import java.util.Objects;
+
+public class Person {
+    String name;
+    String surname;
+    int age;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return age == person.age && name.equals(person.name) && surname.equals(person.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, age);
+    }
+}
